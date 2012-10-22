e512c272745855606794c72357e1f11bd674ac85
torianin <tori@robert-i.com>
2012-10-22 23:58:26 +0200

    Working card shop

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,5 +3,13 @@ class OrdersController < ApplicationController
 		@buy_item = Product.find(params[:id])
 		@current_buyer = Buyer.find(@current_user)
 		@current_buyer.update_attributes(params[:current_buyer])
+		@o = @current_buyer.orders.new
+		@o.buyer_id = 3
+		@o.save 		
+		@oi = @o.order_items.new
+		@oi.order_id = @o.id
+		@oi.product_id = @buy_item
+		@oi.quantity = 1
+		@oi.save
 	end
 end
